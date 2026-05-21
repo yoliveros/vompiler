b112ec22a6bb4c98fd3839e7bc8bdae2bf0bf2cd
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2026-05-21 18:43:44 -0400

    arena moved

--- src/main.c
+++ src/main.c
@@ -10,14 +10,11 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  mem_arena *perm_arena = arena_create(GiB(1), MiB(1));
-
   string8 file = STRING8_LIT(argv[1]);
   string8 flags = STRING8_LIT(argv[2]);
 
   b32 succ = driver(flags, file);
 
-  arena_destroy(perm_arena);
   if (succ)
     return 0;
 
