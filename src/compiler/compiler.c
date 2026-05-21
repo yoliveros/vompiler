7cc44bd44eb7d2752f7e2351ad87504e69003b3b
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2026-05-21 18:43:23 -0400

    scratch arena

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -1,7 +1,7 @@
 
-#include "lexer/lexer.c"
-
 string8 compiler(string8 flag, string8 buff) {
+  mem_arena *perm_arena = arena_create(GiB(1), MiB(1));
+
   if (flag.str == nullptr)
     return (string8){0};
 
@@ -10,10 +10,13 @@ string8 compiler(string8 flag, string8 buff) {
   // TODO codegen
   // TODO Assembly
 
-  lexer_init(lexer, buff);
+  mem_arena_temp scratch = arena_scratch_get(nullptr, 0);
+  lexer_init(scratch.arena, lexer, buff);
+
   if (strncmp(flag.str, "--lex", flag.len)) {
     goto out;
   }
+  arena_scratch_release(scratch);
 
   // TODO parser
   if (strncmp(flag.str, "--parse", flag.len)) {
@@ -29,5 +32,6 @@ string8 compiler(string8 flag, string8 buff) {
   }
 
 out:
+  arena_destroy(perm_arena);
   return (string8){0};
 }
